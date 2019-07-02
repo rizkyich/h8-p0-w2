fb24638ea13b92820ab88c7847552d4ddd542c1a
Rizky Ichsandi <icsandirizky@gmail.com>
2019-07-02 09:21:48 +0700

    add exercise 3

---
+++ exercise-3.js
@@ -0,0 +1,24 @@
+// Proxytia Game
+
+var nama = 'Ruby';
+var peran = 'Tabib';
+
+if (nama) {
+  if (peran) {
+    if (peran === 'Ksatria') {
+      console.log('Selamat datang di Dunia Proxytia, ' + nama);
+      console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!')
+    } else if (peran === 'Tabib') {
+      console.log('Selamat datang di Dunia Proxytia, ' + nama);
+      console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka')
+    } else if (peran === 'Tabib') {
+      console.log('Selamat datang di Dunia Proxytia, ' + nama);
+      console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
+    }  
+  } else {
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+  }  
+} else {
+  console.log('Nama harus diisi!');
+}
+
