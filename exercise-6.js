dfd8b615a637c939133501c5ec3c996016c3802a
Rizky Ichsandi <icsandirizky@gmail.com>
2019-07-03 15:27:33 +0700

    add exercise 6

--- exercise-6.js
+++ exercise-6.js
@@ -45,20 +45,20 @@ for (var i = 1; i <= 100; i++) {
 console.log('Kelipatan 3, counter 2');
 for (var i = 1; i <= 100; i+=2) {
   if (i % 3 == 0) {
-    console.log('3 KELIPATAN ' + i);
+    console.log(i + ' KELIPATAN 3');
   }
 }
 
 console.log('Kelipatan 6, counter 5');
 for (var i = 1; i <= 100; i+=5) {
   if (i % 6 == 0) {
-    console.log('6 KELIPATAN ' + i);
+    console.log(i + ' KELIPATAN 6');
   }
 }
 
 console.log('Kelipatan 10, counter 9');
 for (var i = 1; i <= 100; i+=9) {
   if (i % 10 == 0) {
-    console.log('10 KELIPATAN ' + i);
+    console.log(i + ' KELIPATAN 10');
   }
 }
