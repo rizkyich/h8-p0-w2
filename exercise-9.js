40e506923f683d66ab8cae058b7a35e4b135890e
Rizky Ichsandi <icsandirizky@gmail.com>
2019-07-03 22:45:47 +0700

    add exercise 8 and 9

---
+++ exercise-9.js
@@ -0,0 +1,35 @@
+// TUGAS 1 
+
+function shoutOut() {
+  return 'Halo Function!'
+}
+
+console.log(shoutOut());
+
+
+// TUGAS 2
+
+function calculateMultiply(a, b) {
+  return a * b;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);
+
+
+// TUGAS 3
+
+function processSentence(name, age, address, hobby) {
+  return'Nama saya ' + name + ', umur saya ' + age + ' tahun, alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!'; 
+}
+
+var name = "Uwi";
+var age = 23;
+var address = "Jln. Jengki, Jakarta";
+var hobby = "dancing";
+
+var fullSentence = processSentence(name, age, address, hobby);
+console.log(fullSentence); 
