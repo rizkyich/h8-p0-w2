279f890ed5904bacfdf0f8deaab2f5761b442663
Rizky Ichsandi <icsandirizky@gmail.com>
2019-07-03 21:57:51 +0700

    add exercise 7

---
+++ exercise-7.js
@@ -0,0 +1,46 @@
+// 1. Menyusun Barisan Bintang
+
+var row1 = 5;
+
+for (var a = 0; a < row1; a++) {
+  console.log('*');
+}
+
+
+//2. Menyusun Barisan Bintang Dengan Nested Looping
+
+var row2 = 5;
+
+for (var i = 0; i < row2; i++) {
+  var star = '';
+  for (var j = 0; j < row2; j++) {
+    star += '*';
+  }
+  console.log(star);
+  star = '';
+}
+
+
+//2. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+var row3 = 5;
+
+for (var k = 0; k < row3; k++) {
+  var star2 = '';
+  for (var l = 0; l <= k; l++) {
+    star2 += '*';    
+  }
+  console.log(star2);
+  star2 = '';
+}
+
+//////////////////////////////////////////
+// Print barisan tangga tanpa nested loop
+// var row3 = 5;
+
+// for (var k = 0; k < row3; k++) {
+//   var star2 = '';
+//   star2 += '*';
+//   console.log(star2);
+// }
+/////////////////////////////////////////
