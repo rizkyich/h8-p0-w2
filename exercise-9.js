4e96f33c0032c47f852ae0392e01f08d3b15bfe8
Rizky Ichsandi <icsandirizky@gmail.com>
2019-07-04 14:00:22 +0700

    fix exercise 9

--- exercise-9.js
+++ exercise-9.js
@@ -23,7 +23,7 @@ console.log(hasilPerkalian);
 // TUGAS 3
 
 function processSentence(name, age, address, hobby) {
-  return'Nama saya ' + name + ', umur saya ' + age + ' tahun, alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!'; 
+  return 'Nama saya ' + name + ', umur saya ' + age + ' tahun, alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + '!'; 
 }
 
 var name = "Uwi";
